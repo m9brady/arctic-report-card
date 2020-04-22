db5ddc613d00a45ab6db1377f6842b99778cc646
Mike Brady <mike.brady@canada.ca>
2020-04-22 17:04:55 -0400

    end of day

--- scripts/snow_fig_2.py
+++ scripts/snow_fig_2.py
@@ -7,33 +7,50 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 import xarray as xr
 
+plt.rcParams['font.weight'] = 'bold'
+plt.rcParams['font.family'] = 'Arial'
+plt.rcParams['axes.labelsize'] = 'x-large'
+plt.rcParams['axes.labelweight'] = 'bold'
+
 fig_crs = ccrs.LambertAzimuthalEqualArea(central_latitude=90, central_longitude=-80)
 data_crs = ccrs.PlateCarree()
-figsize = (15,8)
+figsize = (15, 6.75)
 dpi = 100
 extent = [-6e6, 5.4e6, -5e6, 5e6]
-cmap = mpl.cm.coolwarm
-norm = mpl.colors.Normalize(vmin=-50, vmax=50)
 cbar_ticks = [-50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50]
+# copied the RGB values from Arc18_Snow_Fig2.png
+cmap = mpl.colors.LinearSegmentedColormap.from_list(
+    name='fig2',
+    colors=[
+        (x[0]/255, x[1]/255, x[2]/255) for x in [
+            (  0, 95,206), (  0,150,189), ( 57,195,255),
+            (140,211,255), (198,255,222), (255,255,255),
+            (255,255,156), (255,211, 66), (239,162,  0),
+            (255,113, 57), (255, 47, 34)
+        ]
+    ],
+    N=256
+)
+norm = mpl.colors.BoundaryNorm(cbar_ticks, cmap.N)
 
 data_root = Path(__file__).parent.parent / 'data'
-
 # load netcdf data (snow cover duration, orig type = timedelta)
 with xr.open_dataset(data_root / 'ARC_SCD_anomaly_a.nc') as ds:
     data_a = ds['scd'].astype(float)
 with xr.open_dataset(data_root / 'ARC_SCD_anomaly_b.nc') as ds:
     data_b = ds['scd'].astype(float)
-
 # load vector data
 land = gpd.read_file(data_root / 'vector' / 'land.gpkg').to_crs(fig_crs.proj4_init)
-#lakes = gpd.read_file(data_root / 'vector' / 'lakes.gpkg').to_crs(fig_crs.proj4_init)
+lakes = gpd.read_file(data_root / 'vector' / 'lakes.gpkg').to_crs(fig_crs.proj4_init)
+ac = gpd.read_file(data_root / 'vector' / 'arcticcircle.gpkg').to_crs(fig_crs.proj4_init)
 #rivers = gpd.read_file(data_root / 'vector' / 'rivers.gpkg').to_crs(fig_crs.proj4_init)
 
+# initialize canvas
 fig, (ax_a, ax_b) = plt.subplots(
     figsize=figsize,
     nrows=1,
     ncols=2,
-    subplot_kw={'projection':fig_crs}
+    subplot_kw={'projection': fig_crs}
 )
 
 # setting margins
@@ -42,41 +59,46 @@ plt.subplots_adjust(top=0.99, bottom=0.01, left=0.01, right=0.99, wspace=0, hspa
 # plotting netcdf data
 mesh_a = data_a.plot(
     ax=ax_a, add_colorbar=False, center=0, transform=data_crs, 
-    cmap=cmap, norm=norm
+    cmap=cmap, vmin=-50, vmax=50,
 )
-mesh_b = data_b.plot(
-    ax=ax_b, add_colorbar=False, center=0, transform=data_crs, 
-    cmap=cmap, norm=norm
+mesh_b = data_b.plot.contourf(
+    ax=ax_b, add_colorbar=False, transform=data_crs, 
+    cmap=cmap, norm=norm#vmin=-50, vmax=50,
 )
 
 # setting figure extents
 ax_a.set_extent(extent, crs=fig_crs)
-ax_a.outline_patch.set_linewidth(0.5)
+ax_a.outline_patch.set_linewidth(1)
 ax_b.set_extent(extent, crs=fig_crs)
-ax_b.outline_patch.set_linewidth(0.5)
+ax_b.outline_patch.set_linewidth(1)
 
 # adding vector data to map
 ax_a.add_geometries(land['geometry'], facecolor='None', edgecolor='black', linewidth=0.4, crs=fig_crs)
 ax_b.add_geometries(land['geometry'], facecolor='None', edgecolor='black', linewidth=0.4, crs=fig_crs)
 
-#ax_a.add_geometries(lakes['geometry'], facecolor='white', edgecolor='black', linewidth=0.4, crs=fig_crs)
-#ax_b.add_geometries(lakes['geometry'], facecolor='white', edgecolor='black', linewidth=0.4, crs=fig_crs)
+ax_a.add_geometries(lakes['geometry'], facecolor='white', edgecolor='black', linewidth=0.4, zorder=2, crs=fig_crs)
+ax_b.add_geometries(lakes['geometry'], facecolor='white', edgecolor='black', linewidth=0.4, zorder=2, crs=fig_crs)
+
+ax_a.add_geometries(ac['geometry'], facecolor='None', edgecolor='black', linestyle='dashed', linewidth=1.5, alpha=0.4, zorder=3, crs=fig_crs)
+ax_b.add_geometries(ac['geometry'], facecolor='None', edgecolor='black', linestyle='dashed', linewidth=1.5, alpha=0.4, zorder=3, crs=fig_crs)
 
 #ax_a.add_geometries(rivers['geometry'], edgecolor='black', linewidth=0.15, crs=fig_crs)
 #ax_b.add_geometries(rivers['geometry'], edgecolor='black', linewidth=0.15, crs=fig_crs)
 
-
 # adding colorbars
 caxa = inset_axes(ax_a, width='5%', height='55%', loc=3)
 cb_a = plt.colorbar(mesh_a, cax=caxa, ticks=cbar_ticks, orientation='vertical')
-cb_a.solids.set_edgecolor("face")
+ax_a.text(0.015, 0.59, 'days', fontsize=18, transform=ax_a.transAxes)
+cb_a.ax.tick_params(labelsize=12)
 
 caxb = inset_axes(ax_b, width='5%', height='55%', loc=3)
 cb_b = plt.colorbar(mesh_b, cax=caxb, ticks=cbar_ticks, orientation='vertical')
+ax_b.text(0.015, 0.59, 'days', fontsize=18, transform=ax_b.transAxes)
+cb_b.ax.tick_params(labelsize=12)
 
 # axis text indicators
-ax_a.text(0.02, 0.94, 'a.', family='Arial', fontweight='bold', fontsize=32, transform=ax_a.transAxes)
-ax_b.text(0.02, 0.94, 'b.', family='Arial', fontweight='bold', fontsize=32, transform=ax_b.transAxes)
+ax_a.text(0.02, 0.94, 'a.', fontsize=30, transform=ax_a.transAxes)
+ax_b.text(0.02, 0.94, 'b.', fontsize=30, transform=ax_b.transAxes)
 
 plt.show()
-#plt.savefig(data_root.parent / 'scratch' 'test.png', dpi=300)
+#plt.savefig(data_root.parent / 'scratch' / 'test.png', dpi=dpi)
